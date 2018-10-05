da4ef95f2cb0000198618369bc8f82bc8367ec25
Bryan Lunt <bjlunt2@illinois.edu>
2018-10-05 17:39:39 -0500

    build script also pushes

--- docker/build.sh
+++ docker/build.sh
@@ -10,6 +10,19 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #end snippet
 ##
 
-docker build -t cs484_base ${SCRIPT_DIR}/base
-docker build -t cs484_test ${SCRIPT_DIR}/wgtest
-docker build -t cs484_student ${SCRIPT_DIR}/student
+BASE_VERSION="v0.3"
+TEST_VERSION="v0.3"
+STUD_VERSION="v0.3"
+
+docker build -t cs484_base -t uiuccs484parallelprog/cs484_base:latest -t uiuccs484prallelprog/cs484_base:${BASE_VERSION}  ${SCRIPT_DIR}/base
+docker build -t cs484_test -t uiuccs484parallelprog/cs484_test:latest -t uiuccs484prallelprog/cs484_base:${TEST_VERSION} ${SCRIPT_DIR}/wgtest
+docker build -t cs484_student -t uiuccs484parallelprog/cs484_student:latest -t uiuccs484prallelprog/cs484_student:${STUD_VERSION} ${SCRIPT_DIR}/student
+
+docker push uiuccs484parallelprog/cs484_base:${BASE_VERISON}
+docker push uiuccs484parallelprog/cs484_test:${TEST_VERSION}
+docker push uiuccs484parallelprog/cs484_student:${STUD_VERSION}
+
+docker push uiuccs484parallelprog/cs484_base:latest
+docker push uiuccs484parallelprog/cs484_test:latest
+docker push uiuccs484parallelprog/cs484_student:latest
+
