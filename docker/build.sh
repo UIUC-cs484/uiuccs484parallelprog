37e69fee50c55703b88e98d142cf85fa36019669
Bryan Lunt <bjlunt2@illinois.edu>
2018-10-08 14:34:25 -0500

    Fixed problem with build.sh added newer git to base.

--- docker/build.sh
+++ docker/build.sh
@@ -10,13 +10,13 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #end snippet
 ##
 
-BASE_VERSION="v0.3"
-TEST_VERSION="v0.3"
-STUD_VERSION="v0.3"
+BASE_VERSION="v0.4"
+TEST_VERSION="v0.4"
+STUD_VERSION="v0.4"
 
-docker build -t cs484_base -t uiuccs484parallelprog/cs484_base:latest -t uiuccs484prallelprog/cs484_base:${BASE_VERSION}  ${SCRIPT_DIR}/base
-docker build -t cs484_test -t uiuccs484parallelprog/cs484_test:latest -t uiuccs484prallelprog/cs484_base:${TEST_VERSION} ${SCRIPT_DIR}/wgtest
-docker build -t cs484_student -t uiuccs484parallelprog/cs484_student:latest -t uiuccs484prallelprog/cs484_student:${STUD_VERSION} ${SCRIPT_DIR}/student
+docker build -t cs484_base -t uiuccs484parallelprog/cs484_base:latest -t uiuccs484parallelprog/cs484_base:${BASE_VERSION}  ${SCRIPT_DIR}/base
+docker build -t cs484_test -t uiuccs484parallelprog/cs484_test:latest -t uiuccs484parallelprog/cs484_test:${TEST_VERSION} ${SCRIPT_DIR}/wgtest
+docker build -t cs484_student -t uiuccs484parallelprog/cs484_student:latest -t uiuccs484parallelprog/cs484_student:${STUD_VERSION} ${SCRIPT_DIR}/student
 
 docker push uiuccs484parallelprog/cs484_base:${BASE_VERISON}
 docker push uiuccs484parallelprog/cs484_test:${TEST_VERSION}
