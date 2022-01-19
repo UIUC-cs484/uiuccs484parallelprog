57d8a3afe0226f392c056840170594a9cb4365b7
Bryan Lunt <bjlunt2@illinois.edu>
2022-01-19 16:44:12 -0600

    updated to new version of Ansible, newest gbench and gtest, etc.

--- docker/build.sh
+++ docker/build.sh
@@ -10,13 +10,13 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #end snippet
 ##
 
-BASE_VERSION="v0.7.3"
-TEST_VERSION="v0.7.3"
-CHARM_VERSION="v0.7.3"
-STUD_VERSION="v0.7.3"
+BASE_VERSION="v0.7.4"
+TEST_VERSION="v0.7.4"
+CHARM_VERSION="v0.7.4"
+STUD_VERSION="v0.7.4"
 
 #Base machine, now with everything.
 cp ${SCRIPT_DIR}/../ansible/vmfarm/* ${SCRIPT_DIR}/base/vmfarm/
 docker build -t cs484_base -t uiuccs484parallelprog/cs484_base:latest -t uiuccs484parallelprog/cs484_base:${BASE_VERSION} ${SCRIPT_DIR}/base && \
 rm ${SCRIPT_DIR}/base/vmfarm/*.yml && \
-docker build -t cs484_student -t uiuccs484parallelprog/cs484_student:latest -t uiuccs484parallelprog/cs484_student:${STUD_VERSION} ${SCRIPT_DIR}/student 
+docker build -t cs484_student -t uiuccs484parallelprog/cs484_student:latest -t uiuccs484parallelprog/cs484_student:${STUD_VERSION} ${SCRIPT_DIR}/student
